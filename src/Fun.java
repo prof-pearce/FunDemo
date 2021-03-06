771d544d9bc5a77ff273d0b80f7f7f782bb381d1
prof-pearce <1948155+prof-pearce@users.noreply.github.com>
2021-03-05 16:52:40 -0800

    Changed fun1 & fun2

--- src/Fun.java
+++ src/Fun.java
@@ -1,5 +1,5 @@
 public class Fun {
-    public Integer fun1(Integer x) { return x * x + 5; }
-    public Integer fun2(Integer x) { return 3 * x + 6; }
+    public Integer fun1(Integer x) { return x * x - 6; }
+    public Integer fun2(Integer x) { return 3 * x + 4; }
     public Integer fun3(Integer x) { return x + 2; }
 }
