781a2c67e2c1e7caf9a45b6e54ea4b8d3e8e020c
prof-pearce <1948155+prof-pearce@users.noreply.github.com>
2021-03-05 16:33:05 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Fun fun = new Fun();
+        System.out.println("fun1(10) = " + fun.fun1(10));
+        System.out.println("fun2(10) = " + fun.fun2(10));
+        System.out.println("fun1(10) = " + fun.fun3(10));
+    }
+}
